a117f2fa98e9a1d05a2a88e18577c32b990aed33
arrmdhn9 <ar.rmdhn9@gmail.com>
2024-02-07 06:39:53 +0700

    adding data

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,23 @@ window.onscroll = () => {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// Kontak
+function sendWhatsAppMessage() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Construct WhatsApp message
+  var whatsappMessage =
+    "Nama: " + name + " " + "Email: " + email + " " + "Pesan: " + message;
+
+  // Encode message for URL
+  var encodedMessage = encodeURIComponent(whatsappMessage);
+
+  // WhatsApp URL
+  var whatsappURL = "https://wa.me/6281818199908/?text=" + encodedMessage;
+
+  // Open WhatsApp in new tab
+  window.open(whatsappURL);
+}

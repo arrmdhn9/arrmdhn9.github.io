03724b7015345a871ba34e73af036c74b42b2019
arrmdhn9 <ar.rmdhn9@gmail.com>
2024-02-07 07:14:05 +0700

    finishing before all

--- src/js/script.js
+++ src/js/script.js
@@ -11,11 +11,16 @@ hamburger.addEventListener("click", () => {
 window.onscroll = () => {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
 
@@ -38,3 +43,23 @@ function sendWhatsAppMessage() {
   // Open WhatsApp in new tab
   window.open(whatsappURL);
 }
+
+// Click di luar Hamburger
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// darkmode
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
